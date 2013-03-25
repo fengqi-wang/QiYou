7bfb17fa7120e8a1179ff97eb0db16b624175db1
Fengqi Wang <fengqi.wang@gmail.com>
2013-03-25 23:56:09 +0800

    Version_0.01

---
+++ src/com/laowang/qiyou/Backend.java
@@ -0,0 +1,170 @@
+package com.laowang.qiyou;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.weibo.sdk.android.Oauth2AccessToken;
+import com.weibo.sdk.android.api.WeiboAPI;
+import com.weibo.sdk.android.WeiboException;
+import com.weibo.sdk.android.api.StatusesAPI;
+import com.weibo.sdk.android.net.RequestListener;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Handler;
+import android.os.HandlerThread;
+import android.os.Message;
+import android.util.Log;
+
+
+
+public final class Backend extends HandlerThread {
+	
+	static final int OAUTH = 6100000;
+	static final int RELOAD = 6100001;
+	static final int PREV = 6100002;
+	static final int NEXT = 6100003;
+	static final int LOADING = 6100004;
+	static final int RELOADED = 6100005;
+	
+	static final int AUTHRIZED = 6180000;
+	static final int UPDATE_UI = 6180001;
+	static final int NO_MORE = 6180002;
+	
+
+	public Backend(Handler aHandler) {
+		// TODO Auto-generated constructor stub
+		super("QiYou_Backend");
+		mainHandler = aHandler;
+		start();
+		
+		mWorker = new Handler(this.getLooper()) {
+			@Override
+			 public void handleMessage(Message msg) {
+        	    Log.i("Backend",msg.toString());
+        	    
+           	     switch(msg.what){
+           	     case RELOAD:
+           	    	 acquireNews();
+           	    	 return;
+        	     case RELOADED:
+        	    	 mIndex = 0; 
+        	    	 break;
+        	     case PREV:
+        	    	 if(--mIndex < 0) { mIndex =0; mainHandler.sendEmptyMessage(NO_MORE);}
+         	     	 break;
+        	     case NEXT:
+        	    	 if(++mIndex > (mJsonArray.length()-1)) { mIndex = (mJsonArray.length()-1);  mainHandler.sendEmptyMessage(NO_MORE);}
+        	    	 
+        	    	 break;
+        	     default:
+        	    	 break;
+        	     } 
+          	     
+           	   	 JSONObject jsonObject = (JSONObject)mJsonArray.opt(mIndex);
+                 text = jsonObject.optString("text");
+                 picture = null;
+ 	    	     mainHandler.sendEmptyMessage(UPDATE_UI);
+ 	    	     updatePic();
+ 	    	     mainHandler.sendEmptyMessage(UPDATE_UI);
+           }
+		};
+	}
+	
+	public boolean reload(){
+		mWorker.sendEmptyMessage(RELOAD);
+		return true;
+	}
+	
+	public boolean next(){
+		mWorker.sendEmptyMessage(NEXT);
+		return true;
+	}
+	
+	public boolean prev(){
+		mWorker.sendEmptyMessage(PREV);
+		return true;
+	}
+	
+	private boolean acquireNews()
+	{
+		RequestListener listener = new RequestListener(){
+         	public void onComplete(String response){
+         		Log.i("Backend", response);
+
+         		try
+         		{
+            		JSONObject jsonData = new JSONObject(response);
+         	    	mJsonArray = jsonData.getJSONArray("statuses"); 
+         		}
+         		
+         		catch(JSONException e)
+         		{
+         		    Log.i("Backend", e.toString());         		                           
+         		}	
+         		
+    	    	mWorker.sendEmptyMessage(RELOADED);                  
+         	}
+
+         	public void onIOException(IOException e){Log.i("Backend", "IO problem!!");}
+
+         	public void onError(WeiboException e){Log.i("Backend", e.toString());}
+         };
+         StatusesAPI api = new StatusesAPI(accessToken);
+         //api.update("Test QiTu","","",listener);
+         api.userTimeline("∆Â≈∆–¬Œ≈", (long)0, (long)0, 50, 1, false, WeiboAPI.FEATURE.ALL, false, listener);
+	
+		return true;
+	}
+	
+    private Bitmap returnPic(String url) {   
+    	if(url == "") return null;
+		URL myFileUrl = null;   
+		Bitmap bitmap = null;   
+		
+		try {   
+		myFileUrl = new URL(url);   
+		} catch (Exception e) {   
+		e.printStackTrace();   
+		}   
+		if(myFileUrl == null) return null;
+		try {   
+		HttpURLConnection conn = (HttpURLConnection) 
+		myFileUrl.openConnection();   
+		conn.setDoInput(true);   
+		conn.connect();   
+		InputStream is = conn.getInputStream();   
+		bitmap = BitmapFactory.decodeStream(is);   
+		is.close();   
+		} catch (IOException e) {   
+			return null;  //	e.printStackTrace();   
+		}   
+		return bitmap;   
+	}
+    
+	private void updatePic(){
+		JSONObject jsonObject = (JSONObject)mJsonArray.opt(mIndex);            
+        String picUrl = jsonObject.optString("original_pic");
+        Log.i("Backend",picUrl);     
+
+        if(picUrl.isEmpty()) picUrl = "http://tp2.sinaimg.cn/1730866981/180/1289531929/1";
+		picture = returnPic(picUrl);
+	}
+	
+	private Handler mainHandler;
+	private Handler mWorker;
+	
+	public static Oauth2AccessToken accessToken;
+	
+	private JSONArray mJsonArray;
+	private int mIndex;
+	
+	public static Bitmap picture;
+	public static String text;
+}
---
+++ src/com/laowang/qiyou/QiYouActivity.java
@@ -0,0 +1,219 @@
+package com.laowang.qiyou;
+
+
+import com.weibo.sdk.android.Oauth2AccessToken;
+import com.weibo.sdk.android.Weibo;
+import com.weibo.sdk.android.WeiboAuthListener;
+import com.weibo.sdk.android.WeiboDialogError;
+import com.weibo.sdk.android.WeiboException;
+import com.weibo.sdk.android.keep.AccessTokenKeeper;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.util.Log;
+import android.view.Display;
+import android.view.GestureDetector;
+import android.view.GestureDetector.OnGestureListener;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.OnTouchListener;
+import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class QiYouActivity extends Activity implements OnGestureListener, OnTouchListener {
+	
+	class AuthDialogListener implements WeiboAuthListener {
+	    @Override
+	    public void onComplete(Bundle values) {
+	        String token = values.getString("access_token");
+	        String expires_in = values.getString("expires_in");
+	        Backend.accessToken = new Oauth2AccessToken(token, expires_in);
+	        if (Backend.accessToken.isSessionValid()) {
+	            AccessTokenKeeper.keepAccessToken(QiYouActivity.this,
+	                   Backend.accessToken);
+	            mHandler.sendEmptyMessage(Backend.AUTHRIZED);
+	        }
+	    }
+
+	    @Override
+	    public void onError(WeiboDialogError e) {
+	    
+	    }
+
+	    @Override
+	    public void onCancel() {
+	       
+	    }
+
+	    @Override
+	    public void onWeiboException(WeiboException e) {
+	        
+	    }
+	}
+
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		LayoutInflater inflater = getLayoutInflater();
+		View fm = inflater.inflate(R.layout.activity_qi_you, null);
+		setContentView(fm);
+		
+		//View mv = findViewById(R.id.mainView);
+		fm.setOnTouchListener(this);
+        mGestureDetector = new GestureDetector(this,this);
+        				
+		mHandler = new Handler(){
+			 public void handleMessage(Message msg) {
+				 Log.i("Main", msg.toString());
+				 switch(msg.what){
+				 case Backend.AUTHRIZED:
+					 Toast.makeText(QiYouActivity.this, "认证成功", Toast.LENGTH_SHORT)
+	                 .show();
+					 mBackend.reload();
+					 break;
+				 case Backend.UPDATE_UI:
+					 update();
+					 break;
+				 case Backend.NO_MORE:
+					 Toast.makeText(QiYouActivity.this, "这边没有了", Toast.LENGTH_SHORT)
+	                 .show();
+					 break;
+				 default:
+					 Toast.makeText(QiYouActivity.this, msg.toString(), Toast.LENGTH_SHORT)
+	                 .show();						 
+				 }
+				 
+	           }
+		};
+		
+		mBackend = new Backend(mHandler);
+		mWeibo = Weibo.getInstance(CONSUMER_KEY, REDIRECT_URL);
+		mWeibo.authorize(QiYouActivity.this, new AuthDialogListener());
+		
+		Backend.picture = BitmapFactory.decodeResource(getResources(), R.drawable.sinaweibo);
+		Backend.text = "新浪体育棋牌频道官方微博，专业于各项棋牌类赛事报道";
+	}
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.activity_qi_you, menu);
+		return true;
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		if(item.getItemId() == R.id.menu_refresh)
+			mBackend.reload();
+		return true;
+	}
+	
+	@Override 
+	public boolean onTouch(View v, MotionEvent event) { 	
+	// OnGestureListener will analyzes the given motion event
+	//这里的mGestureDetector是该Activity的一个属性.在构造方法中实例化或在oncreate()方法中实例化.
+	Log.i("Main",event.toString());	
+	return mGestureDetector.onTouchEvent(event); 
+	} 
+	
+	@Override
+	public void onShowPress(MotionEvent e) {
+	} 
+	@Override
+	public boolean onDown(MotionEvent e) {
+	// TODO Auto-generated method stub
+	//	Toast.makeText(this, "onDown", Toast.LENGTH_SHORT).show();
+	return false;
+	} 	
+	@Override
+	public boolean onSingleTapUp(MotionEvent e) {
+	//	Toast.makeText(this, "onSingleTapUp", Toast.LENGTH_SHORT).show();
+	return false;
+	} 
+	@Override
+	public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX,float velocityY) {
+		// TODO Auto-generated method stub
+		 if(e1==null || e2==null) return false;
+		 
+		 final int FLING_MIN_DISTANCE = 100, FLING_MIN_VELOCITY = 200; 
+		 if (e1.getX() - e2.getX() > FLING_MIN_DISTANCE && Math.abs(velocityX) > FLING_MIN_VELOCITY) { 
+		 // Fling left 
+		 Log.i("MyGesture", "Fling left"); 
+		 mBackend.next(); 
+		 } else if (e2.getX() - e1.getX() > FLING_MIN_DISTANCE && Math.abs(velocityX) > FLING_MIN_VELOCITY) { 
+		 // Fling right 
+		 Log.i("MyGesture", "Fling right"); 
+		 mBackend.prev(); 
+		 } else if(e2.getY()-e1.getY()>FLING_MIN_DISTANCE && Math.abs(velocityY)>FLING_MIN_VELOCITY) {
+		 // Fling down 
+		 Log.i("MyGesture", "Fling down"); 
+		 //Toast.makeText(this, "Fling down", Toast.LENGTH_SHORT).show();
+		 } else if(e1.getY()-e2.getY()>FLING_MIN_DISTANCE && Math.abs(velocityY)>FLING_MIN_VELOCITY) {
+		 // Fling up 
+		 Log.i("MyGesture", "Fling up"); 
+		 //Toast.makeText(this, "Fling up", Toast.LENGTH_SHORT).show();
+		 } 
+		 return true; 
+    } 
+	@Override
+	public void onLongPress(MotionEvent e) {
+	// TODO Auto-generated method stub
+	}
+	@Override
+	public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+	// TODO Auto-generated method stub
+	//	Toast.makeText(this, "onScroll", Toast.LENGTH_SHORT).show();
+	return false;
+	} 
+	 
+	
+	private void update()
+	{
+		Bitmap bmp = Backend.picture;
+		if(bmp == null)	{
+			bmp = BitmapFactory.decodeResource(getResources(), R.drawable.loading);
+		}		
+        int bmpwidth = bmp.getWidth();
+		int bmpheight = bmp.getHeight();
+	    int screenWidth;//屏幕宽度 
+	    //int screenHeight;//屏幕高度 
+	    WindowManager windowManager = getWindowManager(); 
+	    Display display = windowManager.getDefaultDisplay(); 
+	    screenWidth = display.getWidth(); 
+	    //screenHeight = display.getHeight(); 
+		
+		float scaleWidth = (float) (screenWidth) / bmpwidth;
+		//float scaleHeight = (float) screenHeight / bmpheight;
+        
+        Matrix matrix = new Matrix();
+		matrix.postScale(scaleWidth,scaleWidth);
+		Bitmap bm = Bitmap.createBitmap(bmp,0,0,bmpwidth,bmpheight ,matrix,true);
+		
+		ImageView iv = (ImageView)findViewById(R.id.imageView);
+		iv.setImageBitmap(bm);
+		
+		TextView tv = (TextView)findViewById(R.id.text);
+		tv.setText(Backend.text);
+	}
+
+	private GestureDetector mGestureDetector;
+	
+	private Backend mBackend;
+	Handler mHandler;
+
+	private Weibo mWeibo;
+	private static final String CONSUMER_KEY = "3756648256"; 
+	private static final String REDIRECT_URL = "http://www.sina.com.cn";
+}
